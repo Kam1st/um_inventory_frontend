8318e1739967fe4745776d5a529ec9befb7dc6ed
Kam1st <kam.hatteea@hotmail.com>
2023-01-11 14:26:48 -0500

    test done for this method, with assert

--- cypress/e2e/Get_Orders_By_Stock_Item_Id.cy.js
+++ cypress/e2e/Get_Orders_By_Stock_Item_Id.cy.js
@@ -2,8 +2,9 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:8081/orders')
     cy.wait(3000)
-    cy.get("#stock-id").type('7486504')
+    cy.get('#stock-id').type('7486504').should("have.value","7486504")
     cy.contains('Get Data By Stock Item Id').click()
+
 
   })
 })
